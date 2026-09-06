eb39996786fde025aad85eb11397da306bf98c4d
Henry Hale <92443116+henryhale@users.noreply.github.com>
2026-09-06 17:43:16 +0300

    feat(theme): add a download component for server releases

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,11 @@
+import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+import DownloadButtons from "./components/DownloadButtons.vue";
 import "./style.css";
 
-export default DefaultTheme;
+export default {
+	extends: DefaultTheme,
+	enhanceApp({ app }) {
+		app.component("DownloadButtons", DownloadButtons);
+	},
+} satisfies Theme;
